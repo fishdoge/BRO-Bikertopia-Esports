71d9a2bbf5c6c37cd82931ae12863ccd589f97cd
unknown <ru19wu06@gmail.com>
2022-02-27 14:29:20 +0800

    update mint

--- js/web3/web3Index.js
+++ js/web3/web3Index.js
@@ -3,27 +3,27 @@ async function login_In_button(){
     window.location.reload();
 }
 
-window.addEventListener('DOMContentLoaded', async () => {
+// window.addEventListener('DOMContentLoaded', async () => {
 
 
-    let account = await web3.eth.getAccounts();
+//     let account = await web3.eth.getAccounts();
    
-    coinbase = account[0];
-    let ETHbalance = await web3.eth.getBalance(coinbase);
-    ETHbalance = web3.utils.fromWei(ETHbalance, 'ether');
+//     coinbase = account[0];
+//     let ETHbalance = await web3.eth.getBalance(coinbase);
+//     ETHbalance = web3.utils.fromWei(ETHbalance, 'ether');
 
-    let add = coinbase.substring(0,5)
-    add = add +"....";
-    $("#wallet-btn").text(add);
-    let add2 = coinbase.substring(0,5)
-    $("#wallet-2").text(add2);
+//     let add = coinbase.substring(0,5)
+//     add = add +"....";
+//     $("#wallet-btn").text(add);
+//     let add2 = coinbase.substring(0,5)
+//     $("#wallet-2").text(add2);
 
-    let add3 = coinbase.substring(0,10);
-    $("#login_address2").text(add3); 
+//     let add3 = coinbase.substring(0,10);
+//     $("#login_address2").text(add3); 
    
  
-    setNFT_ABI();
-})
+//     setNFT_ABI();
+// })
 
 async function chage(){
     let account = await web3.eth.getAccounts();
